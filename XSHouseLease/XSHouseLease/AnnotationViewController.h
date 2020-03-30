e265d19ed61367a217150f82982830452a59a373
xs <xs@Mac-miniChenX.local>
2020-03-30 17:26:02 +0800

    1111

---
+++ XSHouseLease/XSHouseLease/AnnotationViewController.h
@@ -0,0 +1,14 @@
+//
+//  AnnotationViewController.h
+//  Category_demo
+//
+//  Created by songjian on 13-3-21.
+//  Copyright (c) 2013年 songjian. All rights reserved.
+//
+
+
+@interface AnnotationViewController : UIViewController
+@property (nonatomic) CLLocationCoordinate2D  location;
+
+
+@end
